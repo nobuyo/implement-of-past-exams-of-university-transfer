0da1adf7c740b3079504a5b5efe52996ed49b9e3
nobuyo <world_fantasy_2017@live.jp>
2017-06-23 14:30:32 +0900

    fix search_tree()

--- tsukuba/h28_1.c
+++ tsukuba/h28_1.c
@@ -43,6 +43,7 @@ int search_tree(struct node *root, char *key) {
     len = strlen(key);
     struct node *n = root;
     for (i = 0; i < len; i++) {
+        printf("%c\n", key[i]);
         if (n->next_char[numchar(key[i]) - 1] == NULL) {
             return -1;
         } else {
@@ -57,12 +58,12 @@ int search_tree(struct node *root, char *key) {
 }
 
 int main(void) {
-    int i,j,l;
+    int i,j,k,l;
     struct node *root, *n;
 
     // initialize for array
     char str_list[N][10] = {
-        "ant", "anywhere", "apart", "appear", "apple", "bad", "bug", "below", "cry", "cut"
+        "ant", "anyway", "apart", "appear", "apple", "bad", "bag", "below", "cry", "cut"
     };
 
     for (int i = 0; i < N; i++)
@@ -78,31 +79,51 @@ int main(void) {
 
     // initialize for tree
 
-    printf("a\n");
-    root->end_of_key = 0;
-    printf("b\n");
-    root->value = 0;
-    printf("c\n");
-    n = root;
+    root = (struct node *)malloc(sizeof(struct node));
 
-    for (i=0; i<N; i++) {
-        // l = strlen(str_list[i]);
-        j = 0;
-        while (str_list[i][j] != '\0') {
-            printf("%c, %d\n", str_list[i][j], numchar(str_list[i][j]));
-            n->next_char[numchar(str_list[i][j]) - 1] = (struct node *)malloc(sizeof(struct node));
-            n = n->next_char[numchar(str_list[i][j]) - 1];
-            n->end_of_key = 0;
-            n->value = 0;
-            j++;
-        }
-        // n->end_of_key = 1;
-        // n->value = (i+1)*10;
+    root->end_of_key = 0;
+    root->value = 0;
+
+    for (k=0; k<26; k++) {
+        root->next_char[k] = NULL;
     }
 
-    printf("search_array 'bad': %d\n", search_array("bad"));
-    // printf("search_tree 'bad': %d", search_tree(root ,"bad"));
-    // // printf("search 'bad': %d", search_array("bad");
+    for (i=0; i<N; i++) {
+        l = strlen(str_list[i]);
+        j = 0;
+        n = root;
+
+        while (str_list[i][j] != '\0') {
+            // printf("%c, %d\n", str_list[i][j], numchar(str_list[i][j]));
+            if (i == 0 || n->next_char[numchar(str_list[i][j]) - 1] == NULL) {
+                n->next_char[numchar(str_list[i][j]) - 1] = (struct node *)malloc(sizeof(struct node));
+                // for (k='a'; k<='z'; k++) {
+                //     printf("%c, %p ", k, n->next_char[numchar(k)-1]);
+                // }
+                // putchar('\n');
+                n = n->next_char[numchar(str_list[i][j]) - 1];
+                n->end_of_key = 0;
+                n->value = 0;
+                for (k=0; k<26; k++) {
+                    n->next_char[k] = NULL;
+                }
+            }
+            else {
+                // for (k='a'; k<='z'; k++) {
+                //     printf("%c, %p ", k, n->next_char[numchar(k)-1]);
+                // }
+                n = n->next_char[numchar(str_list[i][j]) - 1];
+            }
+
+            j++;
+        }
+        n->end_of_key = 1;
+        n->value = (i+1)*10;
+    }
+
+    printf("search_array '%s': %d\n", "bad", search_array("bad"));
+    printf("search_tree '%s': %d\n",  "anyway", search_tree(root ,"anyway"));
+    // printf("search 'bad': %d", search_array("bad");
 }
 
 
