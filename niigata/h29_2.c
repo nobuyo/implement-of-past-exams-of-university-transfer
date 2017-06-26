f0deb14020ad01c46aeface61c18d61267535224
nobuyo <world_fantasy_2017@live.jp>
2017-06-26 17:27:33 +0900

    create niigata/h29_2.c

--- niigata/h29_2.c
+++ niigata/h29_2.c
@@ -1,58 +1,68 @@
 #include <stdio.h>
-
-int calc(int type[]) {
-    int i;
-    int n = 1;
-    int sum = 0;
-
-    for (i = 0; i <= 8; i++, n++) {  
-        switch(type[i]) {
-        case 0:
-            sum += n;
-            break;
-        case 1:
-            sum -= n;
-            break;
-        case 2:
-            sum += n;
-            break;
-        }
-        if (i == 8) break;
-        if (type[i + 1] == 0) {
-            sum *= 10;
-        }
-    }
-    printf("%d%d%d%d%d%d%d%d%d\n", type[8], type[7], type[6], type[5], type[4], type[3], type[2], type[1], type[0]);
-    printf("%d\n", sum);
-
-    return sum;
-}
+#include <math.h>
 
 int main(void) {
     int i, j;
-    int idx[9];
+    int ope[9], num[9];
+    int tmp, sum;
+    int max = pow(3,9);
 
-    for (idx[0] = 0; idx[0] < 3; idx[0]++) {
-        for (idx[1] = 0; idx[1] < 3; idx[1]++) {
-            for (idx[2] = 0; idx[2] < 3; idx[2]++) {
-                for (idx[3] = 0; idx[3] < 3; idx[3]++) {
-                    for (idx[4] = 0; idx[4] < 3; idx[4]++) {
-                        for (idx[5] = 0; idx[5] < 3; idx[5]++) {
-                            for (idx[6] = 0; idx[6] < 3; idx[6]++) {
-                                for (idx[7] = 0; idx[7] < 3; idx[7]++) {
-                                    for (idx[8] = 0; idx[8] < 3; idx[8]++) {
-                                        // printf("%d%d%d%d%d%d%d%d%d\n", idx[0], idx[1], idx[2], idx[3], idx[4], idx[5], idx[6], idx[7], idx[8]);
-                                        int sum = calc(idx);
-                                        if (sum == 100) {
-                                            printf("%d%d%d%d%d%d%d%d%d\n", idx[0], idx[1], idx[2], idx[3], idx[4], idx[5], idx[6], idx[7], idx[8]);
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
+    for (i = 0; i < max; i++) {
+        sum = 0;
+        tmp = i;
+        for (j = 0; j<9; j++) {
+            ope[j] = tmp%3;
+            tmp /= 3;
+        }
+
+        for (j = 0; j<9; j++) {
+            tmp = 0;
+            if (ope[j] == 0 && j != 0) {
+                if (num[j-1]<0) {
+                    tmp = (j+1) * (-1);
+                }
+                else {
+                    tmp = j+1;
+                }
+                num[j] = num[j-1] * 10 + tmp;
+                num[j-1] = 0;
+            }
+            else if (ope[j] == 0 && j == 0) {
+                num[j] = j+1;
+            }
+            else if (ope[j] == 1) {
+                num[j] = j+1;
+            }
+            else if (ope[j] == 2) {
+                num[j] = (j+1) * (-1);
+            }
+        }
+
+
+        for (j = 0; j<9; j++) {
+            sum += num[j];
+            // printf("%d ", num[j]);
+        }
+        // putchar('\n');
+        // for (j = 0; j<9; j++) {
+        //     printf("%d ", ope[j]);
+        // }
+        // putchar('\n');
+        // putchar('\n');
+
+        if (sum == 100) {
+            for (j = 0; j<9; j++) {
+                if (ope[j] == 0) {
+                    printf("%d", j+1);
+                }
+                else if (ope[j] == 1) {
+                    printf("+%d", j+1);
+                }
+                else if (ope[j] == 2) {
+                    printf("-%d", j+1);
                 }
             }
+            putchar('\n');
         }
     }
     return 0;
